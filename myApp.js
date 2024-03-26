15db4bf9d84d74747c959909a4e78ce10bc4fc05
vivski <vnls1@aluno.ifal.edu.br>
2024-03-25 22:34:46 -0300

    completed
    12/12

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 const logger = require('morgan');
 
+let bodyParser = require('body-parser')
+
 let app = express();
 
 require('dotenv').config()
@@ -10,20 +12,21 @@ require('dotenv').config()
 
 // 2 lição - 
 //app.get("/", (req, res) => {
-//     res.send('Hello Express');
-// });
-
-//4 lição - usar um estilo css na página tambem com o --dirname que possibilita utilizar um arquivo
-app.use("/public", express.static(__dirname + "/public/"))
-
+    //     res.send('Hello Express');
+    // });
+    
+    //4 lição - usar um estilo css na página tambem com o --dirname que possibilita utilizar um arquivo
+    app.use("/public", express.static(__dirname + "/public/"))
+    
+// 11 - usar body-parser para solicitações POST 
+app.use(bodyParser.urlencoded({extended:false}))
 
 // 7 lição - criar um registrador simples que deve logar no console o seguinte formato: method, path e ip 
 // e o next para o servidor não travar utilizando o app.use()
-app.use(logger('dev'));
-app.use((req,res,next)=> {
-    console.log(`${req.method} ${req.path} oi ${req.ip}`)
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
-})   
+  });    
 
 // 3 lição - exibir um arquivo html no servidor com--dirname que informa que se trata de um arquivo
 app.get("/",(req,res) => {
@@ -52,10 +55,49 @@ app.get("/json", (req,res) => {
     }
 
 })
+// 8 - encadeando o middleware para criar um servidor de tempo
+// function obterHorarioAtualString(req,res){
+//     return new Date().toString()
+// }
+app.get("/now",(req,res,next) => {
+    // req.time = obterHorarioAtualString()
+    req.time = new Date().toString()
+    next()
+}, function(req,res){
+    res.json({
+        time: req.time
+    });
+}
+)
+
+// 9 - Obter a entrada do parâmetro de roteamento do client
+// Crie um servidor de eco, 
+// montado na rota GET /:word/echo. Responda com um documento JSON, pegando a estrutura {echo: word}.
+// ao escrever alguma coisa ex http//localhost:3000/oilinda/echo eu vou receber o objeto json
+// {word : oi linda}
+
+app.get("/:word/echo", (req,res) => {
+    res.json({
+      echo : req.params.word
+  })
+})
+// 10 - obter nomes inseridos na URL (duas strings)
+app.get("/name",(req,res) => {
+  
+  res.json({
+    name: req.query.first + " " + req.query.last
+  })
+})
 
 
+// 12 - obs: os nomes das rotas podem ser os mesmos nomes se os métodos forem diferentes
+//
 
-
+app.post("/name",(req,res) => {
+  res.json({
+    name: req.body.first + " " + req.body.last
+  })
+})
 
 
 
